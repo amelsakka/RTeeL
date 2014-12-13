303d4b78433db0808fdc8337cf6c5bff9b06260f
amelsakka <amelsakka@gmail.com>
2014-12-13 17:32:14 +0200

    First commit to Github

---
+++ RTeeL/Letter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RTeeL
+{
+    class Letter
+    {
+        public string Id { get; set; }
+        public string First { get; set; }
+        public string Mid { get; set; }
+        public string Final { get; set; }
+        public string Iso { get; set; }
+        public bool ToPrev { get; set; }
+        public bool ToNext { get; set; }
+        public string LA { get; set; }
+    }
+}
---
+++ RTeeL/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RTeeL - RTL Fix")]
+[assembly: AssemblyDescription("RTeel (R-Tee-L) fixes RTL connected text and convert it to a form that could be properly rendered on systems that does not support RTL or connected letters. It currently supports only Arabic. Please contact me if you are interested in adding other languages (like Parsian) even without any programming skills.\r\n\r\nSpecial Thanks to: Swizzy")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RTeeL")]
+[assembly: AssemblyCopyright("By: Sakka (amelsakka@gmail.com)")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e1ecba5f-d5b0-4518-aba5-e02be04b9bbc")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
